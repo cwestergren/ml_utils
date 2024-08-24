86895cbe16a421b00f993e83871c046cf7fa94a0
cwestergren <cwestergren@users.noreply.github.com>
2024-08-24 13:28:52 +0200

    Update custom_logger.py

--- custom_logger.py
+++ custom_logger.py
@@ -2,9 +2,24 @@ import logging
 import sys
 
 def setup_custom_logger(name, log_to_screen=False, log_to_file=True, loglevel=logging.INFO):
-    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
-                                  datefmt='%Y-%m-%d %H:%M:%S')
-        
+    
+    class MultiLineFormatter(logging.Formatter):
+        def format(self, record):
+            # Call the original format method to get the initial message
+            original_message = super().format(record)
+            
+            # Split the message by newlines
+            lines = original_message.splitlines()
+            
+            # Format each line separately
+            formatted_lines = [self._fmt % dict(asctime=self.formatTime(record, self.datefmt),
+                                                levelname=record.levelname,
+                                                message=line) for line in lines]
+            
+            # Join all the formatted lines back together
+            return "\n".join(formatted_lines)
+    
+    formatter = MultiLineFormatter(fmt='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(name)
     logger.setLevel(loglevel)
     
@@ -20,5 +35,4 @@ def setup_custom_logger(name, log_to_screen=False, log_to_file=True, loglevel=lo
         logger.addHandler(file_handler)
     
     return logger
-
 logger = setup_custom_logger('reactlog', log_to_file=False, log_to_screen=True)
